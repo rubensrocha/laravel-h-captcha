e31f6886e0190030eacaca14d3dfff6064f19eaf
Rubens <10297656+rubensrocha@users.noreply.github.com>
2020-09-13 15:32:04 -0300

    Renaming environment variables
    I renamed the environment variables to avoid possible conflicts with other captcha packages that may use the same names, if more than one package is used in the same Laravel installation (hcaptcha + captcha, etc.).

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
  * Secret key and Site key get on https://dashboard.hcaptcha.com/sites
  * */
 return [
-    'secret' => env('CAPTCHA_SECRET', 'default_secret'),
-    'sitekey' => env('CAPTCHA_SITEKEY', 'default_sitekey'),
+    'secret' => env('HCAPTCHA_SECRET', 'default_secret'),
+    'sitekey' => env('HCAPTCHA_SITEKEY', 'default_sitekey'),
     // \GuzzleHttp\Client used is the default client
     'http_client' => \Buzz\LaravelHCaptcha\HttpClient::class,
     'options' => [
